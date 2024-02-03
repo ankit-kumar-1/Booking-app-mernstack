1ae3797e36a94c1258e00421e592126bb9edc2a3
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-02-03 22:37:44 +0530

    authentication

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import IndexPage from './components/IndexPage'
+import LoginPage from './components/LoginPage'
+import Layout from './Layout'
+import RegisterPage from './components/RegisterPage'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:4000'
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ client/src/components/IndexPage.jsx
@@ -0,0 +1,10 @@
+
+function IndexPage() {
+    return (
+        <div>
+            <p>index page here</p>
+        </div>
+    )
+}
+
+export default IndexPage
