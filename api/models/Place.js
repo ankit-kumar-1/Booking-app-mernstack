9760b0d66a15620b49ae40e5c0ae256179c985df
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-02-06 11:28:59 +0530

    place form added

---
+++ api/models/Place.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    chectIn: Number,
+    checkOut: Number,
+    maxGuest: Number,
+
+});
+
+const PlaceModel = mongoose.model('Place', placeSchema);
+module.exports = PlaceModel;
+
