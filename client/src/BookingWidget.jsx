8a2553bf2dce34ba006976e5a6952a8e31267d4d
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-02-16 23:01:05 +0530

    booking functionality

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,7 +1,17 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react"
+import { differenceInCalendarDays } from "date-fns"
+
 
 export default function BookingWidget({ place }) {
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [guests, setGuests] = useState(1);
+    let numberOfNights = 0;
+    if (checkIn && checkOut) {
+        numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+    }
     return (
         <div>
             <div className=" bg-white shadow p-4 rounded-2xl">
@@ -12,20 +22,25 @@ export default function BookingWidget({ place }) {
                     <div className="flex">
                         <div className="py-3 px-4">
                             <label>Check in : </label>
-                            <input type="date" />
+                            <input className=" cursor-pointer" type="date" value={checkIn} onChange={e => setCheckIn(e.target.value)} />
                         </div>
                         <div className="py-3 px-4 border-l">
                             <label>Check out : </label>
-                            <input type="date" />
+                            <input className=" cursor-pointer" type="date" value={checkOut} onChange={e => setCheckOut(e.target.value)} />
                         </div>
                     </div>
                     <div className="py-3 px-4 border-t">
                         <label className=" ml-1">Guests</label>
-                        <input type="number" value={1} />
+                        <input type="number" value={guests} onChange={e => setGuests(e.target.value)} />
                     </div>
                 </div>
 
-                <button className=" primary mt-4 font-bold hover:shadow-lg">Book this Place</button>
+                <button className=" primary mt-4 font-bold hover:shadow-lg">
+                    Book this Place
+                    {numberOfNights > 0 && (
+                        <span> &#x20B9; {numberOfNights * place.price}</span>
+                    )}
+                </button>
             </div>
         </div>
     )
